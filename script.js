b4d7b0fa220039310e44f06473566606408adbf4
ChaDown <charliedownes@hotmail.com>
2022-08-09 14:29:38 +0100

    Finished logic for rock paper scissors, including prompts and a win/lose alert.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const getComputerChoice = function () {
   const num = Math.trunc(Math.random() * 3) + 1;
-  console.log(num);
   let choice;
   if (num === 1) {
     choice = "rock";
@@ -11,32 +10,59 @@ const getComputerChoice = function () {
   } else {
     choice = "scissors";
   }
-  console.log(choice);
   return choice;
 };
 
-const computerSelection = getComputerChoice();
+let userScore = 0;
+let compScore = 0;
 
-const game = function (playerSelection, computerSelection) {
+const playRound = function (playerSelection, computerSelection) {
   const playerSelectionLowerCase = playerSelection.toLowerCase();
+  //   if (playerSelectionLowerCase !== "rock" || "paper" || "scissors") {
+  //     return;
+  //   }
 
   if (playerSelectionLowerCase === computerSelection) {
-    return `It's a draw, you both chose ${playerSelectionLowerCase}. Go again!`;
+    return console.log(
+      `It's a draw, you both chose ${playerSelectionLowerCase}. Go again!`
+    );
   } else if (
     (playerSelectionLowerCase === "paper" && computerSelection === "rock") ||
     (playerSelectionLowerCase === "scissors" &&
       computerSelection === "paper") ||
     (playerSelectionLowerCase === "rock" && computerSelection === "scissors")
   ) {
-    return `You win. ${playerSelectionLowerCase} beats ${computerSelection}`;
+    userScore++;
+    return console.log(
+      `You win. ${playerSelectionLowerCase} beats ${computerSelection}`
+    );
   } else if (
     (playerSelectionLowerCase === "scissors" && computerSelection === "rock") ||
     (playerSelectionLowerCase === "paper" &&
       computerSelection === "scissors") ||
     (playerSelectionLowerCase === "rock" && computerSelection === "paper")
   ) {
-    return `You lose! ${playerSelectionLowerCase} loses to ${computerSelection}`;
+    compScore++;
+    return console.log(
+      `You lose! ${playerSelectionLowerCase} loses to ${computerSelection}`
+    );
   }
 };
 
-console.log(game("Rock", computerSelection));
+const game = function () {
+  for (let i = 0; i < 100; i++) {
+    playRound(prompt("Rock, paper, scissors?"), getComputerChoice());
+    console.log(`Your score ${userScore} : Computer score ${compScore}`);
+    if (userScore + compScore === 5) {
+      break;
+    }
+  }
+  if (userScore > compScore) {
+    alert("You are the champion!");
+  }
+  if (userScore < compScore) {
+    alert("You are a loser!");
+  }
+};
+
+game();
